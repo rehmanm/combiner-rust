02081eac6918ba5beb1f703af7236f12344a9175
Muhammad Abdul Rehman <rehmanm@users.noreply.github.com>
2022-09-22 20:17:12 +0000

    stage 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod args;
+use args::Args;
+use image::{io::Reader, DynamicImage, ImageFormat};
+use std::{fs::File, io::BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::new();
+    println!("{:?}", args);
+    let (image_1, image_format_1) = find_image_from_path(args.image_1);
+    let (image_2, image_format_2) = find_image_from_path(args.image_2);
+}
+
+fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
+    let image_reader: Reader<BufReader<File>> = Reader::open(path).unwrap();
+    let image_format: ImageFormat = image_reader.format().unwrap();
+    let image: DynamicImage = image_reader.decode().unwrap();
+    (image, image_format)
 }
